0a490ac18bfacff8fa4d5b65cd3f202a3a5febf4
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-02-12 16:13:55 +0000

    completion of Intro-05 assignment in swfit, linter errors not fixed yet (of needed)

---
+++ MathOperators.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Ioana Marinescu
+//  Created on 2024/02/12
+//  Version 1.0
+//  Copyright (c) 2024 Ioana Marinescu. All rights reserved.
+//
+//  A program that prints math operations.
+
+// Prints math operations
+print("Addition: 2 + 2 = \(2 + 2)")
+print("Subtraction: 2 - 2 = \(2 - 2)")
+print("Multiplication: 2 * 3 = \(2 * 3)")
+print("Division: 6 / 2 = \(6 / 2)")
+print("More Division: 2 / 3 = \(2 / Float(3))")
