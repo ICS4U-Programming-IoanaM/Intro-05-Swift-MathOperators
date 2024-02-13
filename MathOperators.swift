3b68c46df91500b5ef087edf6314ae39e3bb67f9
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-02-13 15:24:15 +0000

    Added exponential and square root equations

--- MathOperators.swift
+++ MathOperators.swift
@@ -8,9 +8,14 @@
 //
 //  A program that prints math operations.
 
+import Foundation
+
 // Prints math operations
 print("Addition: 2 + 2 = \(2 + 2)")
 print("Subtraction: 2 - 2 = \(2 - 2)")
 print("Multiplication: 2 * 3 = \(2 * 3)")
 print("Division: 6 / 2 = \(6 / 2)")
 print("More Division: 2 / 3 = \(2 / Float(3))")
+print("Even More Division: 2 / 3 = \(2 / Double(3))")
+print("Exponential: 2^3 = \(pow(2, 3))")
+print("Square Root: √4 = \(Int(sqrt(4)))")
